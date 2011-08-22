500cea17b4e8ea1947697079b88167e5788e5e21
seanoshea <oshea.ie@gmail.com>
2011-08-21 17:52:34 -0700

    Adding some memoization to java implementations.

--- Three.java
+++ Three.java
@@ -1,35 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Three {
 
-	private static int[] values = { 3, 4, 9, 14, 15, 19, 28, 37, 47, 50, 54,
-			56, 59, 61, 70, 73, 78, 81, 92, 95, 97, 99 };
+    /**
+     * Memoization cache.
+     */
+    private static Map<String, Integer> cache = new HashMap<String, Integer>();
 
-	public static void main(String[] args) {
-		int sum = count();
-		System.out.println("summ is " + sum);
-	}
+    private static int[] values = { 3, 4, 9, 14, 15, 19, 28, 37, 47, 50, 54,
+	    56, 59, 61, 70, 73, 78, 81, 92, 95, 97, 99 };
 
-	private static int count() {
-		int previousValue = -1, duplicates = 0, totalsets = 0, value;
-		for (int i = 0, l = values.length; i < l; i++) {
-			value = values[i];
-			if (previousValue == value) {
-				duplicates++;
-			}
-			previousValue = value;
-			totalsets += waysToSum(value, 0);
-		}
-		return totalsets - values.length + duplicates;
-	}
+    public static void main(String[] args) {
+	int sum = count();
+	System.out.println("sum is " + sum);
+    }
 
-	private static int waysToSum(int sum, int index) {
-		int value = values[index];
-		if (sum < value) {
-			return 0;
-		} else if (sum > value) {
-			return waysToSum(sum - value, index + 1)
-					+ waysToSum(sum, index + 1);
-		} else {
-			return 1;
-		}
+    private static int count() {
+	int previousValue = -1, duplicates = 0, totalsets = 0, value;
+	for (int i = 0, l = values.length; i < l; i++) {
+	    value = values[i];
+	    if (previousValue == value) {
+		duplicates++;
+	    }
+	    previousValue = value;
+	    totalsets += waysToSum(value, 0);
 	}
+	return totalsets - values.length + duplicates;
+    }
+
+    private static int waysToSum(int sum, int index) {
+	String key = String.valueOf(sum) + "::" + String.valueOf(index);
+	if (!cache.containsKey(key)) {
+	    int value = values[index];
+	    if (sum < value) {
+		cache.put(key, 0);
+	    } else if (sum > value) {
+		cache.put(key, waysToSum(sum - value, index + 1)
+			+ waysToSum(sum, index + 1));
+	    } else {
+		cache.put(key, 1);
+	    }
+	}
+	return cache.get(key);
+    }
 }
--- Two.java
+++ Two.java
@@ -1,61 +1,72 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Two {
 
-	public static void main(String[] args) {
-		int x = nextPrimeFibonacciNumber(227000);
-		int sum = sumPrimeDivisors(x + 1);
-		System.out.println("Sum of prime divisidors for " + (x + 1) + " is "
-				+ sum);
-	}
+    /**
+     * Memoization cache.
+     */
+    private static Map<Integer, Integer> cache = new HashMap<Integer, Integer>();
 
-	private static boolean isPrime(int num) {
-		boolean prime = true;
-		// a few simple cases
-		if (num == 1 || num <= 3) {
-			prime = true;
-		} else {
-			// start iterating and see if anything is divisible
-			for (int i = 2; i <= Math.ceil(num / 2); i++) {
-				if (num % i == 0) {
-					prime = false;
-					break;
-				}
-			}
-		}
-		return prime;
-	}
+    public static void main(String[] args) {
+	int x = nextPrimeFibonacciNumber(227000);
+	int sum = sumPrimeDivisors(x + 1);
+	System.out.println("Sum of prime divisidors for " + (x + 1) + " is "
+		+ sum);
+    }
 
-	private static int fibonacci(int num) {
-		if (num <= 2) {
-			return 1;
-		} else {
-			return fibonacci(num - 1) + fibonacci(num - 2);
+    private static boolean isPrime(int num) {
+	boolean prime = true;
+	// a few simple cases
+	if (num == 1 || num <= 3) {
+	    prime = true;
+	} else {
+	    // start iterating and see if anything is divisible
+	    for (int i = 2; i <= Math.ceil(num / 2); i++) {
+		if (num % i == 0) {
+		    prime = false;
+		    break;
 		}
+	    }
 	}
+	return prime;
+    }
 
-	private static int nextPrimeFibonacciNumber(int min) {
-		int i = 1, fib;
-		// while (true) is a little scary here - might be an idea to
-		// have some kind of interrupt should this while loop run indefinitely
-		while (true) {
-			fib = fibonacci(i);
-			// cheaper to check for less than first
-			// and then worry about primes.
-			if (min < fib && isPrime(fib)) {
-				break;
-			}
-			i++;
-		}
-		return fib;
+    private static int fibonacci(int num) {
+	if (num <= 2) {
+	    return 1;
+	} else {
+	    if (cache.get(num) == null) {
+		cache.put(num, fibonacci(num - 1) + fibonacci(num - 2));
+	    }
+	    return cache.get(num);
 	}
+    }
 
-	private static int sumPrimeDivisors(int num) {
-		int factors = 0, n = num;
-		for (int i = 2; i <= n; i++) {
-			while (n % i == 0) {
-				factors += i;
-				n /= i;
-			}
-		}
-		return factors;
+    private static int nextPrimeFibonacciNumber(int min) {
+	int i = 1, fib;
+	// while (true) is a little scary here - might be an idea to
+	// have some kind of interrupt should this while loop run indefinitely
+	while (true) {
+	    fib = fibonacci(i);
+	    // cheaper to check for less than first
+	    // and then worry about primes.
+	    if (min < fib && isPrime(fib)) {
+		break;
+	    }
+	    i++;
 	}
+	return fib;
+    }
+
+    private static int sumPrimeDivisors(int num) {
+	int factors = 0, n = num;
+	for (int i = 2; i <= n; i++) {
+	    while (n % i == 0) {
+		factors += i;
+		n /= i;
+	    }
+	}
+	return factors;
+    }
 }
